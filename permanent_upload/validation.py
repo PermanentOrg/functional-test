df1942d080982a48ce52cf1f8518f3f14262e750
Cristina Muñoz <hi@xmunoz.com>
2021-10-14 15:14:10 -0500

    Add headers to validation dataset
    Also, improve file extension parsing.

--- permanent_upload/validation.py
+++ permanent_upload/validation.py
@@ -8,16 +8,18 @@ def validate_supported_types(results, data_file="data/supported_file_types.csv")
         os.path.dirname(os.path.realpath(__file__)), data_file
     )
     with open(data_file_path, "r") as csvfile:
-        validation_reader = csv.reader(csvfile)
+        validation_reader = csv.DictReader(csvfile)
         for row in validation_reader:
-            validation_dataset[row[0]] = row[1:]
+            validation_dataset[row["file_extension"]] = row
     for result in results:
-        extension = result[0].split(".")[1]
+        extension = result[0].split(".")[-1]
         assert validation_dataset[extension]
         # File type classification (e.g. audio, image)
-        assert validation_dataset[extension][0] == result[1]
+        assert validation_dataset[extension]["file_category"] == result[1]
         # Final status (e.g. ok, manual_review)
-        assert validation_dataset[extension][1] in [result[2], "uploaded"]
-        if validation_dataset[extension][1] != "uploaded":
+        assert validation_dataset[extension]["final_status"] in set(
+            [result[2], "uploaded", "ok"]
+        )
+        if validation_dataset[extension]["final_status"] != "uploaded":
             # File conversions (e.g. html,pdfa,txt)
-            assert validation_dataset[extension][2] == result[3]
+            assert validation_dataset[extension]["conversions"] == result[3]
